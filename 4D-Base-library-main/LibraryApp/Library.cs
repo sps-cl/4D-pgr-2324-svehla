d369c908f5435f1e257ec8dc189589c6c2b59ba2
mates56214 <matyasvehla@email.cz>
2023-10-02 14:53:25 +0200

    práce ze dne 2.10.2023

---
+++ 4D-Base-library-main/LibraryApp/Library.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryApp.exceptions;
+using LibraryApp.models;
+
+namespace LibraryApp
+{
+    internal class Library<T> where T : LibraryItem<T>
+    {
+        List<T> items = new List<T>();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void ListItems()
+        {
+            Console.WriteLine("knihovni predmety: ");
+            foreach (T item in items)
+            {
+                item.DisplayInfo();
+
+            }
+
+        }
+        public void CheckoutItems(int id)
+        {
+            T itemToCheckout = items.Find(item => item.Id == id);
+            if (itemToCheckout != null)
+            {
+                if (itemToCheckout.IsAvailable)
+                {
+                    itemToCheckout.IsAvailable = false;
+                    Console.WriteLine($"vypujčit si knihu  {itemToCheckout.Title}");
+
+                }
+                else
+                {
+                    throw new NotAvailableException($"položka s ID $ {itemToCheckout.Id} nebyla nalezena");
+                }
+            }
+            else
+            {
+                throw new NotFoundException();
+            }
+            }
+
+        }
+    }
+}
---
+++ 4D-Base-library-main/LibraryApp/exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.exceptions
+{
+    internal class NotFoundException : Exception
+    {
+        public NotFoundException(string message = "kniha nebyla nalezene") : base(message) { }  
+    }
+}
